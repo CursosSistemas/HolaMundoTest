8f33733d765cee8ce73019182a403c14470a2292
Michael Asturias <asturias_m@hotmail.com>
2020-10-17 12:37:39 -0600

    Commit1
    Commit1

---
+++ HolaMundoTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundoTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+        public static String createMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
